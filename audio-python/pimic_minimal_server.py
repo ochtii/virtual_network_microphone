6467821fa0b4e07ce6a42e1c831179f39f9e3bf2
ochtii <ochtii@users.noreply.github.com>
2025-09-24 22:02:32 +0200

    Add debug logging for RTP start POST data

--- audio-python/pimic_minimal_server.py
+++ audio-python/pimic_minimal_server.py
@@ -1381,6 +1381,7 @@ class HTTPHandler(SimpleHTTPRequestHandler):
                 return
             
             post_data = self.rfile.read(content_length)
+            logger.info(f"RTP start POST data received: {post_data}")
             data = json.loads(post_data.decode('utf-8'))
             
             client_ip = data.get('client_ip')
